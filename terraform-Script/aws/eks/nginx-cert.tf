2df49fda12264614f3d39889b8164a44608c1e4d
Yugandharkumar <yugaa22@gmail.com>
2023-08-04 18:34:42 +0530

    Create nginx-cert.tf

---
+++ terraform-Script/aws/eks/nginx-cert.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "nginx" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace = "ingress-nginx"
+  create_namespace = "true"
+  set {
+    name  = "apiService.create"
+    value = "true"
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "jetstack"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace = "cert-manager"
+  create_namespace = "true"
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
