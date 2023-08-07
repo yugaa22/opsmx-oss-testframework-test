d7d20743ede7cc0c349e5cd415886488b3a43b0f
Yugandharkumar <yugaa22@gmail.com>
2023-08-07 12:28:36 +0530

    Update nginx-cert.tf

--- terraform-Script/aws/eks/nginx-cert.tf
+++ terraform-Script/aws/eks/nginx-cert.tf
@@ -1,9 +1,19 @@
+
+
 provider "helm" {
   kubernetes {
-    config_path = "kubeconfig"
-
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
 
   }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
 }
 
 
