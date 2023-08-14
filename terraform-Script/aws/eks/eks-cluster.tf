dfff87f3da8d1f9c1219ebafad441c659edfd150
Lalit Vikram Singh <lalit@opsmx.io>
2023-08-14 10:52:01 +0530

    Update eks-cluster.tf (#2)

--- terraform-Script/aws/eks/eks-cluster.tf
+++ terraform-Script/aws/eks/eks-cluster.tf
@@ -78,3 +78,47 @@ resource "aws_security_group_rule" "cluster_outbound" {
   to_port                  = 65535
   type                     = "egress"
 }
+
+//////
+
+data "tls_certificate" "cluster" {
+  url = aws_eks_cluster.this.identity.0.oidc.0.issuer
+}
+
+### OIDC config
+resource "aws_iam_openid_connect_provider" "cluster_aws_iam_openid_connect_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  #thumbprint_list = []
+  thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity.0.oidc.0.issuer
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  #role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  role_name                     = aws_eks_cluster.this.name
+  provider_url                  = try(replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", ""), null)
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+}
